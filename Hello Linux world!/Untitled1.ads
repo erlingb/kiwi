cb44d1f8c264de29f415cb6eded1f831fc737b3b
erlingb <54189264+erlingb@users.noreply.github.com>
2019-09-26 16:36:38 +0200

    Add files via upload

---
+++ Hello Linux world!/Untitled1.ads
@@ -0,0 +1,8 @@
+/* Prints a message on the screen.   */
+#include <stdio.h>
+#include <stdlib,h
+int main ()
+{
+    printf("Hello Linux World!");
+    return 0;
+}
---
+++ practicea/main.c
@@ -0,0 +1,13 @@
+/*output 3 data types.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("I am learning the %c programming language\n", 'c');
+    printf("I have just completed chapter %d\n", 2);
+    printf("I am %.1f percent ready to move on ", 99.9);
+    printf("to the next chapter!\n");
+    return 0;
+}
